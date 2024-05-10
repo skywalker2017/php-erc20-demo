522a0102dac6450d3b3e8c228b9bbb1bce7ed40b
skywalker2017 <1394375146@qq.com>
2024-05-10 10:29:45 +0800

    add instruction

---
+++ README.md
@@ -0,0 +1,1 @@
+php src/App.php
--- src/App.php
+++ src/App.php
@@ -5,12 +5,13 @@ require 'vendor/autoload.php';
 
 use Lessmore92\Ethereum\Token;
 
-$owner_private = 'a8877005528c1853f2b05af838f643eb6722216cf962bb6d21827b76b89db83e';
-$owner_address = '0x91ebe1163b6576a0ebc8Bf01b2DAEC0DD72769Db';
+// $contractAddress, $url, $ownerPrivate, $ownerAddress 参数可以取自配置文件
+// $contractAddress: 合约地址，上线时被调用方提供
+// $url: 区块链节点url， 被调用方提供
+// $ownerAddress: 调用方钱包地址， 参考： https://zhuanlan.zhihu.com/p/112285438?from_voters_page=true
+// $ownerPrivate: 调用方钱包私钥，参考： https://support.metamask.io/managing-my-wallet/secret-recovery-phrase-and-private-keys/how-to-export-an-accounts-private-key/
+// $toAddress: 用户钱包地址， 用户提供
 
-$to_address = '0x8dc53847EcebBEB7AE9AB5158D4C425B6AE6Dd02';
-// $contractAddress, $url, $ownerPrivate, $ownerAddress, $toAddress 参数可以取自配置文件
-// 
 function erc20Transfer($contractAddress, $url, $ownerPrivate, $ownerAddress, $toAddress) {
     $token = new \Lessmore92\Ethereum\Token($contractAddress, $url);
 
